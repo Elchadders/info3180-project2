18ffd4f02443be03a4f9b4ef1e1cbb22db0c635e
Garth Davy <davychd@yahoo.com>
2017-04-29 00:37:05 +0000

    Finished creating database on heroku

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Web Development Project 2
 
 Group Members:
 
-Garth Davy
-Kemati Morgan
+Garth Davy,
+Kemati Morgan,
 Alex Williams 
--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:password@localhost/musketeers"
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:password@localhost/musketeers"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://cdmzfoncmoxpkz:bf997798c28f70567096e0d8fadd7e2fb8e6a1e5bca651f15baacf6a3d69789a@ec2-54-225-118-55.compute-1.amazonaws.com:5432/d1h8sc4k9uc6ls"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['USER_UPLOAD_FOLDER'] = "./app/static/uploads/users/"
 
